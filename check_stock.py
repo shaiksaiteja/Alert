8a8c017cc7f042dacdd81173879a0bc4882b9828
Shaik Saiteja <109669265+shaiksaiteja@users.noreply.github.com>
2026-06-07 07:45:56 +0530

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -1,15 +1,23 @@
 import requests
 import os
 
+URL = "https://www.sheinindia.in/s/footwear-207316?query=%3Anewn%3Arelevance%3Aundefined%3Aundefined%3Anull&curated=true&curatedid=footwear-207316&customerType=Existing&gridColumns=5&sort=prce-desc&segmentIds=&customertype=Existing"
+
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
+
+response = requests.get(URL, timeout=30)
+
+count = response.text.count("goods_id")
+
+msg = f"🔍 SHEIN Test\nCurrent Count: {count}"
 
 requests.get(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     params={
         "chat_id": CHAT_ID,
-        "text": "✅ GitHub Actions test successful!"
+        "text": msg
     }
 )
 
-print("Message sent")
+print(msg)
